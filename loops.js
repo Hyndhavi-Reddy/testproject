142d00c7898f17bd7342d3203fcf22d13fa219f2
Hyndhavi-Reddy <reddyhyndhavi27@gmail.com>
2024-05-22 16:04:33 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,16 @@
+//for in
+console.log("FOR IN");
+const a = [1,2,3,4,5];
+for(i in a){
+    console.log(a[i])
+}
+console.log("FOR EACH");
+//for each
+a.forEach(function (number) {
+    console.log(number); 
+  });
+  
+  console.log("FOR OF");
+  for(const i of a){
+    console.log(i);
+  }
